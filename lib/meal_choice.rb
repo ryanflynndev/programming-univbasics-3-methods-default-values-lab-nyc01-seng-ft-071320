70c1f1229d393f63525c5f45a15530b6e219f44e
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-03 17:30:17 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,9 +2,7 @@
 # For output purposes, use "puts" instead of "print" or "p"
 def meal_choice (veg1, veg2, protein = "tofu")
   puts "What a nutritious meal!"
-  if veg1 == "carrots" && veg2 == "string beans" && protein == "lentils"
-    puts "What a nutritious meal!"
-  elsif veg1 == "carrots" && veg2 == "string beans" 
+  if veg1 == "carrots" && veg2 == "string beans" && (protein == "lentils" || protein == "tofu")
     puts "What a nutritious meal!"
   end
   return puts "A plate of #{protein} with #{veg1} and #{veg2}."
